5eb547b8f78594f4cf0be11808dc1f049ff68910
helwyr392 <helwyr392@gmail.com>
2018-01-13 21:40:49 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+// Requires and Uses
+const express    = require('express'),
+      app        = express(),
+      mongoose   = require('mongoose'),
+      port       = process.env.port || 8080;
+
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
+
+/*------ Database Setup ------*/
+
+// Database connection
+mongoose.connect('mongodb://helwyr392:D!0n2OO2Noob1025@ds141657.mlab.com:41657/strength_finder', { useMongoClient: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+// Schema
+const strengthSchema = new mongoose.Schema({
+  title: String,
+  subtitle: String,
+  adjectives: String,
+  quote: String,
+  cite: String,
+  yuck: String,
+  yay: String,
+  starved: String,
+  fed: String,
+  insulted: String,
+  honored: String,
+  working: String,
+  managing: String,
+  partnering: String,
+  imgUrl: String,
+  color: String,
+  colorHover: String,
+  colorRGBA: String
+});
+
+// Model
+var Strength = mongoose.model('strength', strengthSchema);
+
+/*------ Routes ------*/
+
+// Index - GET
+app.get('/', (req, res) => {
+    Strength.find({ 'title' : 'Analytical' }, (err, result) => {
+    if (err) throw err;
+    res.render('index', {strength: result[0]});
+  });
+});
+
+// Strength
+app.get('/:id', (req, res) => {
+    Strength.find({ 'title' : req.params.id }, (err, result) => {
+    if (err) throw err;
+    res.render('index', {strength: result[0]});
+  });
+});
+
+app.listen(port, () => {
+  console.log('Server is running... Port:8080');
+});
---
+++ public/scripts/scripts.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+  
+  /* Variables */
+  var $window   = $(window);
+  var $nav      = $('nav');
+  var brandPos  = $('.js--section-personal-brand').position();
+  var quotePos  = $('.js--performance-last-child').position(); 
+  
+  navToggle();
+
+  /* "Learn More" button scroll */
+  $('.js--scroll-down').click(function() {
+    $('html, body').animate({scrollTop: $('.js--section-personal-brand').offset().top}, 1000);
+  });
+  
+  /* Sticky Navigation Menu */  
+  $window.scroll(function() {
+    var windowPos   = $window.scrollTop();
+    
+    if (!$nav.hasClass('mobile-nav')) {
+      if (windowPos >= brandPos.top - 50) {
+          $nav.addClass('sticky'); 
+      } else {
+        $nav.removeClass('sticky');
+      }
+    }
+  });
+  
+  $window.resize(function() {
+    navToggle();
+  });
+  
+  $('.js--nav-icon').click(function () {
+    if ($('.js--nav-icon i').hasClass('ion-navicon-round')) {
+      $('.mobile-nav li').css('display', 'block');
+      $('.js--nav-icon i').removeClass('ion-navicon-round');
+      $('.js--nav-icon i').addClass('ion-close-round');
+    } else {
+      $('.mobile-nav li').css('display', 'none');
+      $('.js--nav-icon i').removeClass('ion-close-round');
+      $('.js--nav-icon i').addClass('ion-navicon-round');
+    }
+  });
+  
+  function navToggle() {
+    if($window.width() > 900) {
+      $nav.addClass('main-nav');
+      $('.mobile-nav li').css('display', 'inline-block')
+      $nav.removeClass('mobile-nav');
+    } else {
+      $nav.addClass('mobile-nav');
+      $nav.removeClass('main-nav');
+      $('.mobile-nav li').css('display', 'none');
+    }
+  }
+  
+});
